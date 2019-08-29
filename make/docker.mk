4bbf049cdca79b409d412e203c28e266d30c7aa2
Duan Shiqiang <duanshiqiang1@gmail.com>
2019-08-29 14:15:34 +0800

    Add option USE_KANIKO for future use

--- make/docker.mk
+++ make/docker.mk
@@ -8,6 +8,18 @@ export DOCKER_BUILDKIT=1
 docker-build:
 	docker build --progress=plain $(BUILD_ARGS) -t $(DOCKER_REPO)/$(IMAGE_NAME) .
 
+.PHONY: docker-kaniko-build
+docker-kaniko-build: 
+	docker run --rm -v $(CURDIR):/workspace \
+	-v ~/.docker:/kaniko/.docker \
+	gcr.io/kaniko-project/executor:latest \
+	--dockerfile Dockerfile --context dir:///workspace/ \
+	--no-push --tarPath /workspace/image.tar \
+	--cache=true --cache-repo docker.io/$(DOCKER_REPO)/$(IMAGE_NAME) \
+	--destination docker.io/$(DOCKER_REPO)/$(IMAGE_NAME) $(BUILD_ARGS)
+	docker load --input $(CURDIR)/image.tar
+	rm -f $(CURDIR)/image.tar
+
 .PHONY: docker-publish
 docker-publish: ## Publish the image with short commit hash as tag
 	docker tag $(DOCKER_REPO)/$(IMAGE_NAME) $(DOCKER_REPO)/$(IMAGE_NAME):$(COMMIT_SHA)
